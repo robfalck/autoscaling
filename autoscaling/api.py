53b03d8161a842b5dd516649a731329f33c24620
Weyandt <hweyandt@ndc.nasa.gov>
2019-07-25 15:24:03 -0400

    Laid groundwork for automatic isoscaling functionality

--- autoscaling/api.py
+++ autoscaling/api.py
@@ -88,6 +88,44 @@ class PJRNScaler(object):
             print(key)
 
 
+class IsoScaler(object):
+    def __init__(self, jac, lbs, ubs):
+        self.refs = {}
+        self.ref0s = {}
+        self.defect_refs = {}
+
+        vnames = set()
+        for of, wrt in jac:
+            if '.states:' in wrt:
+                vnames.add(wrt)
+            elif '.controls:' in wrt:
+                vnames.add(wrt)
+
+        fnames = set()
+        for of, wrt in jac:
+            if '.defects:' in of:
+                fnames.add(of)
+
+        Kv_inv = {v: ubs[v] - lbs[v] for v in vnames}
+
+        Kf_inv = {}
+        for f in fnames:
+            key = None
+            loc_f = f.split(':')[-1]
+            for v in vnames:
+                if v.split(':')[-1] == loc_f:
+                    key = v
+                    break
+            assert(key is not None)
+            Kf_inv[f] = Kv_inv[key]  # note that this is NOT an array!
+
+        for nm in vnames:
+            self.refs[nm] = ubs[nm]
+            self.ref0s[nm] = lbs[nm]
+        for nm in fnames:
+            self.defect_refs[nm] = Kf_inv[nm]
+
+
 def pjrnscale(prob, jac, lbs, ubs):
     sc = PJRNScaler(jac, lbs, ubs)
     set_refs(prob.model, sc)
